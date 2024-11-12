36d66e951fe8bb864c34a4460b8a807db3d93a36
Mohammed Aqeeb <mohammedaqeeb19@gmail.com>
2024-11-12 23:22:08 +0530

    components,usestates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextArea from './components/TextArea';
+
+import React ,{useState} from 'react';
 
 function App() {
+
+  const [mode, switchMode] = useState('light');
+
+  const changeMode = () =>{
+    if(mode === 'light'){
+      switchMode('dark')
+    }
+    else{
+      switchMode('light')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar titleName='TextUtils' homeTitle='Home' aboutTitle='About' modes={mode}  toggelMode={changeMode}/>
+    <div className="container my-3">
+      <TextArea headLine='Enter the text to change'/>
     </div>
+    {/* <About/> */}
+    </>
   );
 }
 
---
+++ src/components/TextArea.js
@@ -0,0 +1,67 @@
+
+
+import React, {useState} from 'react'
+
+export default function TextArea(props) {
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+    const [text,setText] = useState('');
+
+    const OnClickUpperCase = (event) =>{
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+
+    const onClickLowerCase = (event) =>{
+        let lowerText = text.toLowerCase();
+        setText(lowerText);
+    }
+
+    const onClickClearText = (event) => {
+        let clearText = '';
+        setText(clearText);
+    }
+
+    const handleCopyText = (event) => {
+        var copyText = document.getElementById('exampleFormControlTextarea1');
+        copyText.select();
+        navigator.clipboard.writeText(copyText.value);
+    }
+
+    const handleSpaces =() =>{
+        const spaceText = text.split(/[ ]+/);
+        setText(spaceText.join(' '));
+    }
+
+
+  return (
+    <>
+    <div>
+        <div className="mb-3">
+        <h1>{props.headLine}</h1>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="6" value={text}
+            onChange={handleOnChange}
+         ></textarea>
+        </div>
+        <button type="button" className="btn btn-primary mx-1" onClick={OnClickUpperCase} >Convert To UpperCase</button>
+        <button type="button" className="btn btn-primary mx-2" onClick={onClickLowerCase} >Convert to LowerCase</button>
+        <button type="button" className="btn btn-primary mx-2" onClick={handleCopyText} >Copy Text</button>
+        <button type="button" className="btn btn-primary mx-2" onClick={handleSpaces} >Remove Extra Space</button>
+        <button type="button" className="btn btn-primary mx-2" onClick={onClickClearText} >Clear</button>
+    </div>
+
+
+    <div className="container my-4">
+        <h2>Your Text Summary</h2>
+        <p>Total Number of Words <b>{text.split(' ').length}</b> and Characters <b>{text.length}</b></p>
+        <p>{0.008 * text.split(' ').length} Minutes To read</p>
+        <h2>Text Preview</h2>
+        <p>{text}</p>
+
+    </div>
+    </>
+  )
+}
